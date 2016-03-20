1772af407c225a77197a73f6240dfecc2abdeef9
Xeloras <Xeloras@gmail.com>
2016-03-20 00:35:51 -0400

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
+		self.browser.refresh()
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
@@ -58,9 +59,9 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		#Noew a new user, Frandcis, comes along to the site.
+		self.browser.refresh()
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
-		time.sleep(3)
 		#Francis visits the home page. There is no sign of Edith's list.
 		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
