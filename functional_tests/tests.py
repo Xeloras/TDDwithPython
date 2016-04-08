86ade3cb4cb214a2c0fa95a30649e8f3a3648301
Xeloras <Xeloras@gmail.com>
2016-04-07 22:01:08 -0400

    Add gunicorn to virtualenv requirements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,11 +14,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
 				cls.server_url = 'http://' + arg.split('=')[1]
 				return
 		super().setUpClass()
-		cls.server_url = cls.live_server_url
+		cls.server_url = super().live_server_url
 
 	@classmethod
 	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
+		if cls.server_url == super().live_server_url:
 			super().tearDownClass()
 
 
